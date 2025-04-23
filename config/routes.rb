e42ae1711bb8cdadc4692c983cd2f410e2895ffc
LAPTOP-J3HDT85H\maap3 <maap.3dymori@gmail.com>
2025-04-23 22:13:16 +0900

    Revert "Webページを表示"
    This reverts commit d97ab0d0e2f2dbea7f34e425931a81ec747c7a0e.

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,13 @@
 Rails.application.routes.draw do
-
-  root to: 'home#index'
-  get '/about' => 'home#about'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
-  # 一旦非表示 get "up" => "rails/health#show", as: :rails_health_check
+  get "up" => "rails/health#show", as: :rails_health_check
 
   # Render dynamic PWA files from app/views/pwa/*
-  # 一旦非表 get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
-  # 一旦非表 get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
+  get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
+  get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
 
   # Defines the root path route ("/")
   # root "posts#index"
