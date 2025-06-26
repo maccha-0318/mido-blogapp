4a07fecd70ab7758ead92ced53b6d74f73519d08
LAPTOP-J3HDT85H\maap3 <maap.3dymori@gmail.com>
2025-06-26 22:58:59 +0900

    createアクション

--- config/application.rb
+++ config/application.rb
@@ -14,7 +14,8 @@ module MidoBlogapp
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
-    config.autoload_lib(ignore: %w[assets tasks])
+    #config.autoload_lib(ignore: %w[assets tasks])非表示にした
+    config.i18n.default_locale = :ja
 
     # Configuration for the application, engines, and railties goes here.
     #
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   root to: 'articles#index'
 
-  resources :articles, only: [:show]
+  resources :articles, only: [:show, :new, :create]
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
