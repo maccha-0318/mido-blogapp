08c2ab7a6fdd167b72033856499b3d0640c42442
LAPTOP-J3HDT85H\maap3 <maap.3dymori@gmail.com>
2025-07-05 21:47:16 +0900

    記事を更新できるように変更

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   root to: 'articles#index'
 
-  resources :articles, only: [:show, :new, :create]
+  resources :articles, only: [:show, :new, :create, :edit, :update]
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
